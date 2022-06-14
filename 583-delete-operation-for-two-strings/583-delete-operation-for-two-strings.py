045b6b87060c39653811302848f1c0ebbe618475
Trevor <trevorguidry6@gmail.com>
2022-06-13 21:54:39 -0400

    Time: 1005 ms (5.11%), Space: 70.3 MB (6.26%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        
+        N1 = len(word1)
+        N2 = len(word2)
+        
+        @cache
+        def backtrack(index1, index2):
+            
+            if (index1 == N1) and (index2 == N2):
+                return 0
+            
+            ans = inf
+            if index1 < N1 and index2 < N2 and word1[index1] == word2[index2]:
+                ans = backtrack(index1+1, index2+1)
+                
+            if index1 < N1:
+                ans = min(ans, backtrack(index1+1, index2) + 1)
+                
+            if index2 < N2:
+                ans = min(ans, backtrack(index1, index2+1) + 1)
+            
+            return ans
+            
+        return backtrack(0, 0)
+                
+                
+        
+        
+        
+        
