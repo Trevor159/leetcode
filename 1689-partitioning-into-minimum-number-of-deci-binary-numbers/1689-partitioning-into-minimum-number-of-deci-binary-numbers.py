c2b8f722c90e767ab4e824e16a3622a9ade45905
Trevor <trevorguidry6@gmail.com>
2022-06-26 20:36:30 -0400

    Time: 68 ms (90.98%), Space: 14.7 MB (50.25%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,9 +1,5 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        ans = n[0]
-        
-        for i in range(1, len(n)):
-            char = n[i]
-            ans = max(ans, char)
+
             
-        return max(1, int(ans))
+        return int(max(n))
