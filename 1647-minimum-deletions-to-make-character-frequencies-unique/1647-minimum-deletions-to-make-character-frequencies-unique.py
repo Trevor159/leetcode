9f461986c07496f8651ed7c3848ac34cba9bcaac
Trevor <trevorguidry6@gmail.com>
2022-06-27 22:39:10 -0400

    Time: 440 ms (18.16%), Space: 14.7 MB (93.89%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        counts = [0] * 26
+        
+        for c in s:
+            counts[ord(c) - ord("a")] += 1
+            
+        counts.sort(reverse=True)
+        
+        prev = float("inf")
+        ans = 0
+        amount = 0
+        # print(counts)
+        for i in range(26):
+            num = counts[i]
+            nextNum = counts[i+1] if i != 25 else 0
+            
+            if num < prev:
+                prev = num
+                continue
+                
+            amount += 1
+            
+            while amount and prev > nextNum:
+                ans += amount
+                prev -= 1
+                amount -= 1
+            
+            
+        while amount and prev and prev-1 > num:
+            ans += amount
+            amount -= 1
+            prev -= 1
+                
+                
+        return ans
+        
+        
+        
+        
