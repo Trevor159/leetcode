3f7689424c8d61adbc5656133b9c21d95b5fab01
Trevor <trevorguidry6@gmail.com>
2022-07-04 04:02:23 -0400

    Time: 272 ms (39.19%), Space: 16.8 MB (37.74%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -6,18 +6,18 @@ class Solution:
         for i in range(N-2, -1, -1):
             if nums[i] > nums[i+1]:
                 dpright[i] = dpright[i+1] + 1
-                
-                
-        dpleft = [1] * N
+        
+        ans = dpright[0]
+        prev = 1
+
         for i in range(1, N):
             if nums[i] > nums[i-1]:
-                dpleft[i] = dpleft[i-1] + 1
+                prev = prev + 1
+            else:
+                prev = 1
                 
+            ans += max(dpright[i], prev)
                 
-        ans = 0
-        
-        for i in range(N):
-            ans += max(dpleft[i], dpright[i])
             
         return ans
         
